a0137afb28a2ae866252169d1ed81d253807a01c
Maarten Breddels <maartenbreddels@gmail.com>
2018-12-03 10:47:26 +0100

    use modern script to get modern chromium

--- docs/source/chrome.sh
+++ docs/source/chrome.sh
@@ -1,40 +1,30 @@
-# how do you like to call this program? (chrome-linux, chrome, chromium, google-chrome?)
-APPNAME="chromium"
-# get the latest build number (JSON)
-VERSION=$(curl http://commondatastorage.googleapis.com/chromium-browser-continuous/Linux/LAST_CHANGE)
-# get some extra metadata to display on the command line
-LOGURL="http://commondatastorage.googleapis.com/chromium-browser-continuous/Linux/"$VERSION"/REVISIONS"
-LOG=$(curl $LOGURL)
-# build the URL of the file we'll be downloading
-DOWNLOADURL="http://commondatastorage.googleapis.com/chromium-browser-continuous/Linux/"$VERSION"/chrome-linux.zip"
+LASTCHANGE_URL="https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2FLAST_CHANGE?alt=media"
 
+REVISION=$(curl -s -S $LASTCHANGE_URL)
 
+echo "latest revision is $REVISION"
 
-# Here's some command line output ...
-echo -e "\r\n-------------------------"
-echo "BUILD: "$VERSION
-echo $LOGURL
-echo $LOG
-echo -e "-------------------------\r\n"
+if [ -d $REVISION ] ; then
+  echo "already have latest version"
+  exit
+fi
 
-# if this was already downloaded, then it would either need to be deleted or aborted
+ZIP_URL="https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F$REVISION%2Fchrome-linux.zip?alt=media"
 
-# where should we put this 37MB download?
-tempfile="/tmp/chrome-linux-"$VERSION".zip"
-tempdir="."
-# just putting this in my home directory now because i'm lazy
-#permdir=~/$APPNAME"/"$VERSION
+ZIP_FILE="${REVISION}-chrome-linux.zip"
 
-# get the large download ...
-echo "curl $DOWNLOADURL -O $tempfile"
-curl $DOWNLOADURL -o $tempfile
-# mkdir -p $tempdir
-unzip -o $tempfile -d $tempdir
-pwd
-ls -al
-ls -al ${tempdir}/chrome-linux/
-ls -al ${tempdir}/chrome-linux/chrome
-${tempdir}/chrome-linux/chrome --headless --remote-debugging-port=9222
+echo "fetching $ZIP_URL"
+
+rm -rf $REVISION
+mkdir $REVISION
+pushd $REVISION
+curl -# $ZIP_URL > $ZIP_FILE
+echo "unzipping.."
+unzip $ZIP_FILE
+popd
+rm -f ./latest
+ln -s $REVISION/chrome-linux/ ./latest
+./latest/chrome --headless --remote-debugging-port=9222 &
 #exec ${tempdir}/chrome-linux/chrome --headless --remote-debugging-port=9222
 
 
