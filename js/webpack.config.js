229b2109154638ca1855e0c84d222f75b4a45c1b
Maarten Breddels <maartenbreddels@gmail.com>
2018-12-05 13:46:49 +0100

    do not use thread-loader

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -10,13 +10,13 @@ var rules = [
     // { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/},
     { test: /\.(ts|js)?$/, use: [
          { loader: 'cache-loader' },
-         {
-                    loader: 'thread-loader',
-                    options: {
-                        // there should be 1 cpu for the fork-ts-checker-webpack-plugin
-                        workers: require('os').cpus().length - 1,
-                    },
-         },
+        //  {
+        //             loader: 'thread-loader',
+        //             options: {
+        //                 // there should be 1 cpu for the fork-ts-checker-webpack-plugin
+        //                 workers: require('os').cpus().length - 1,
+        //             },
+        //  },
          { loader: "ts-loader", options: {transpileOnly: true,happyPackMode: true} }
         ]}
 ];
