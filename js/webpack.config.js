069dd8e6cbff3174809f2958d71b8b719408263c
Maarten Breddels <maartenbreddels@gmail.com>
2018-12-05 15:28:24 +0100

    use production without source map to save on memory

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -26,86 +26,93 @@ var resolve =  {
 };
 
 
-module.exports = [
-    {// Notebook extension
-     //
-     // This bundle only contains the part of the JavaScript that is run on
-     // load of the notebook. This section generally only performs
-     // some configuration for requirejs, and provides the legacy
-     // "load_ipython_extension" function which is required for any notebook
-     // extension.
-     //
-        entry: './src/extension.js',
-        devtool: 'inline-source-map',
-        output: {
-            filename: 'extension.js',
-            path: path.resolve(__dirname, `../${pyname}/static`),
-            libraryTarget: 'amd'
+
+module.exports = (env, argv) => {
+    let IS_PRODUCTION = false;
+    if (argv.mode === 'production') {
+        console.log('Looks like we are in production mode!');
+        IS_PRODUCTION = true;
+    }
+    // const devtool = IS_PRODUCTION ? undefined : 'inline-source-map',
+    const devtool = IS_PRODUCTION ? undefined : 'source-map'
+    return [
+        {// Notebook extension
+        //
+        // This bundle only contains the part of the JavaScript that is run on
+        // load of the notebook. This section generally only performs
+        // some configuration for requirejs, and provides the legacy
+        // "load_ipython_extension" function which is required for any notebook
+        // extension.
+        //
+            entry: './src/extension.js',
+            devtool: devtool,
+            output: {
+                filename: 'extension.js',
+                path: path.resolve(__dirname, `../${pyname}/static`),
+                libraryTarget: 'amd'
+            },
+            resolve: resolve
         },
-        resolve: resolve
-    },
-    {// Bundle for the notebook containing the custom widget views and models
-     //
-     // This bundle contains the implementation for the custom widget views and
-     // custom widget.
-     // It must be an amd module
-     //
-        entry: './src/index.js',
-        devtool: 'inline-source-map',
-        output: {
-            filename: 'index.js',
-            path: path.resolve(__dirname, `../${pyname}/static`),
-            libraryTarget: 'amd'
+        {// Bundle for the notebook containing the custom widget views and models
+        //
+        // This bundle contains the implementation for the custom widget views and
+        // custom widget.
+        // It must be an amd module
+        //
+            entry: './src/index.js',
+            devtool: devtool,
+            output: {
+                filename: 'index.js',
+                path: path.resolve(__dirname, `../${pyname}/static`),
+                libraryTarget: 'amd'
+            },
+            module: {
+                rules: rules
+            },
+            externals: ['three', 'jupyter-js-widgets', '@jupyter-widgets/base', '@jupyter-widgets/controls'],
+            resolve: resolve
         },
-        devtool: 'source-map',
-        module: {
-            rules: rules
+        {// Embeddable ipyvolume bundle
+        //
+        // This bundle is generally almost identical to the notebook bundle
+        // containing the custom widget views and models.
+        //
+        // The only difference is in the configuration of the webpack public path
+        // for the static assets.
+        //
+        // It will be automatically distributed by unpkg to work with the static
+        // widget embedder.
+        //
+        // The target bundle is always `dist/index.js`, which is the path required
+        // by the custom widget embedder.
+        //
+            entry: './src/embed.js',
+            resolve: {
+                extensions: ['.ts', '.js', '']
+            },
+            output: {
+                filename: 'index.js',
+                path: path.resolve(__dirname, './dist/'),
+                libraryTarget: 'amd',
+                publicPath: 'https://unpkg.com/ipyvolume@' + version + '/dist/'
+            },
+            devtool: IS_PRODUCTION ? 'cheap-source-map' : 'inline-source-map',
+            module: {
+                rules: rules
+            },
+            externals: ['jupyter-js-widgets', '@jupyter-widgets/base', '@jupyter-widgets/controls'],
+            resolve: resolve
         },
-        externals: ['three', 'jupyter-js-widgets', '@jupyter-widgets/base', '@jupyter-widgets/controls'],
-        resolve: resolve
-    },
-    {// Embeddable ipyvolume bundle
-     //
-     // This bundle is generally almost identical to the notebook bundle
-     // containing the custom widget views and models.
-     //
-     // The only difference is in the configuration of the webpack public path
-     // for the static assets.
-     //
-     // It will be automatically distributed by unpkg to work with the static
-     // widget embedder.
-     //
-     // The target bundle is always `dist/index.js`, which is the path required
-     // by the custom widget embedder.
-     //
-        entry: './src/embed.js',
-        devtool: 'inline-source-map',
-        resolve: {
-            extensions: ['.ts', '.js', '']
+        {
+            entry: 'three',
+            output: {
+                filename: 'three.js',
+                path: path.resolve(__dirname, `../${pyname}/static`),
+                libraryTarget: 'amd'
+            },
+            module: {
+                rules: rules
+            }
         },
-        output: {
-            filename: 'index.js',
-            path: path.resolve(__dirname, './dist/'),
-            libraryTarget: 'amd',
-            publicPath: 'https://unpkg.com/ipyvolume@' + version + '/dist/'
-        },
-        devtool: 'source-map',
-        module: {
-            rules: rules
-        },
-        externals: ['jupyter-js-widgets', '@jupyter-widgets/base', '@jupyter-widgets/controls'],
-        resolve: resolve
-    },
-    {
-        entry: 'three',
-        output: {
-            filename: 'three.js',
-            path: path.resolve(__dirname, `../${pyname}/static`),
-            libraryTarget: 'amd'
-        },
-        devtool: 'source-map',
-        module: {
-            rules: rules
-        }
-    },
-];
+    ];
+}
